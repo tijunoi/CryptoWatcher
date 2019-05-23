48a436bebaf15f5b00f0570566a58ab526766c73
Nil Ordoñez <nilordonez.7@gmail.com>
2019-05-23 20:29:27 +0200

    Add redux-offline, including redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,20 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
+import { offline } from '@redux-offline/redux-offline'
+//Use default config as standard persistence is enough
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+//compose with empty options as redux-devtools and redux-offline have typescript typings mismatch
+const compose = composeWithDevTools({})
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(logger),
+        offline(offlineConfig)
+    )
+)
 
 export default store
